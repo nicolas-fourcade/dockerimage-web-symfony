23cb756ac19faa0b627a0d103c81f91940b27322
Nicolas Fourcade <nicolas_fourcade@hotmail.com>
2018-10-15 00:28:57 +0200

    install node and yarn

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+apt update && apt install yarn -y
 mkdir -p app/cache my_project/app/logs
 touch app/logs/prod.log
 touch app/logs/dev.log
