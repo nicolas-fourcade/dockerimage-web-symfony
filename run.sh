e89e1c8c6f3a87cda22ecb287567bae34ab86b30
Fourcade <nicolas_fourcade@hotmail.com>
2018-10-15 02:52:57 +0200

    modification log directory

--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-mkdir -p app/cache my_project/app/logs
+apt install nodejs
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+apt-get update && apt-get install yarn -y
+mkdir -p app/cache /app/logs
 touch app/logs/prod.log
 touch app/logs/dev.log
 chgrp -R www-data .
-chmod -R g+w app/cache my_project/app/logs
+chmod -R g+w app/cache /app/logs
 source /etc/apache2/envvars
 tail -F /var/log/apache2/* app/logs/prod.log app/logs/dev.log &
 exec apache2 -D FOREGROUND
